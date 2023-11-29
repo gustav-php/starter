1d3fe50f031c11f6e9ff3ea7f6b3182f96bc6949
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-11-29 14:13:27 +0100

    fix: remove unnecessary constructor

--- src/Middlewares/Logger.php
+++ src/Middlewares/Logger.php
@@ -7,10 +7,6 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class Logger extends Base
 {
-    public function __construct()
-    {
-    }
-
     public function handle(ServerRequestInterface $request): ServerRequestInterface
     {
         $this->log("[{$request->getMethod()}] {$request->getUri()->getPath()}");
