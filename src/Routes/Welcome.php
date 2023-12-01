ab773250e2ad5de7336fca8028c0ad20a7f97135
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-12-01 16:20:52 +0100

    update to latest release

--- src/Routes/Welcome.php
+++ src/Routes/Welcome.php
@@ -5,6 +5,7 @@ namespace App\Routes;
 use App\Middlewares\Logger;
 use App\Services\Jokes;
 use DI\Attribute\Inject;
+use Exception;
 use GustavPHP\Gustav\Attribute\{
     Middleware,
     Route
@@ -20,7 +21,8 @@ class Welcome extends Controller\Base
     #[Route('/about')]
     public function about()
     {
-        return $this->view(__DIR__ . '/../../views/about.latte');
+        throw new Exception('asd');
+        return $this->view('about.latte');
     }
 
     #[Route('/api')]
@@ -34,7 +36,7 @@ class Welcome extends Controller\Base
     {
         $joke = $this->jokes->jokes[array_rand($this->jokes->jokes)];
 
-        return $this->view(__DIR__ . '/../../views/joke.latte', [
+        return $this->view('joke.latte', [
             'joke' => $joke
         ]);
     }
@@ -42,6 +44,6 @@ class Welcome extends Controller\Base
     #[Route('/')]
     public function welcome()
     {
-        return $this->view(__DIR__ . '/../../views/index.latte');
+        return $this->view('index.latte');
     }
 }
